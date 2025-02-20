6f5c47e72d5932edc9b8771c67e675ee4b0ef778
netrahindocha <netrahindocha1@gmail.com>
2025-02-20 01:28:25 -0500

    Fixing redirect URL for successful login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -41,7 +41,7 @@ const Login = () => {
       });
       console.log(response.data);
       if (response.data.status === "SUCCESS") {
-        navigate("/");
+        navigate("/recording");
         console.log(response.data.status);
         console.log(response.data.message);
       } else if (response.data.status) {
